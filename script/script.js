640ab243ad743894faf577fb20da06f32ee43b45
Raihan <raihanuddinahmed10@gmail.com>
2025-07-01 12:55:40 +0600

    complete

--- script/script.js
+++ script/script.js
@@ -61,11 +61,6 @@ serviceContainer.innerHTML = services
   .map((service) => generateServiceHTML(service))
   .join("");
 
-// get current year for footer section
-
-// const currentYear = new Date().getFullYear();
-// // Set the year inside the span with id="year"
-// document.getElementById("year").textContent = currentYear;
 
 
 const currentYear = new Date().getFullYear();
